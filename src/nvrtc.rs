885f472fb467625bf8e7bfed3cb6c2ea0a251e28
Anders Langlands <anderslanglands@gmail.com>
2018-12-29 09:16:55 +1300

    Fix error string buffer length

--- src/nvrtc.rs
+++ src/nvrtc.rs
@@ -149,8 +149,8 @@ impl Drop for Program {
 }
 
 fn create_whitespace_cstring(len: usize) -> CString {
-    let mut buffer: Vec<u8> = Vec::with_capacity(len as usize + 1);
-    buffer.extend([b' '].iter().cycle().take(len as usize));
+    let mut buffer: Vec<u8> = Vec::with_capacity(len as usize);
+    buffer.extend([b' '].iter().cycle().take(len as usize-1));
     unsafe { CString::from_vec_unchecked(buffer) }
 }
 
