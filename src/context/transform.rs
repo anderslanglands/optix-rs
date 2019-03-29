0f8183d405f4f36a51789fe0da424d5d539f324a
Anders Langlands <anderslanglands@gmail.com>
2019-03-29 20:57:25 +1300

    add function to create with srt motion keys

--- src/context/transform.rs
+++ src/context/transform.rs
@@ -114,6 +114,91 @@ impl Context {
         Ok(xform)
     }
 
+    pub fn transform_create_with_srt_motion(
+        &mut self,
+        motion_keys: &[f32],
+        time_begin: f32,
+        time_end: f32,
+        child: TransformChild,
+    ) -> Result<TransformHandle> {
+        // validate first
+        match child {
+            TransformChild::Group(h) => self.group_validate(h)?,
+            TransformChild::GeometryGroup(ggh) => {
+                self.geometry_group_validate(ggh)?
+            }
+            TransformChild::Transform(th) => self.transform_validate(th)?,
+            TransformChild::None => unreachable!(),
+        }
+
+        let (rt_xform, result) = unsafe {
+            let mut rt_xform: RTtransform = std::mem::zeroed();
+            let result = rtTransformCreate(self.rt_ctx, &mut rt_xform);
+            (rt_xform, result)
+        };
+        if result != RtResult::SUCCESS {
+            return Err(self.optix_error("rtTransformCreate", result));
+        }
+
+        let xform = self.ga_transform_obj.insert(rt_xform);
+
+        let n = motion_keys.len() / 16;
+        let result = unsafe {
+            rtTransformSetMotionKeys(
+                rt_xform,
+                n as u32,
+                MotionKeyType::SRT_FLOAT16,
+                motion_keys.as_ptr() as *const f32,
+            )
+        };
+        if result != RtResult::SUCCESS {
+            return Err(self.optix_error("rtTransformSetMotionKeys", result));
+        }
+
+        let result = unsafe {
+            rtTransformSetMotionRange(rt_xform, time_begin, time_end)
+        };
+        if result != RtResult::SUCCESS {
+            return Err(self.optix_error("rtTransformSetMotionRange", result));
+        }
+
+        match child {
+            TransformChild::Group(h) => {
+                let child_rt_grp = self.ga_group_obj.get(h).unwrap();
+                let result = unsafe {
+                    rtTransformSetChild(rt_xform, *child_rt_grp as RTobject)
+                };
+                if result != RtResult::SUCCESS {
+                    return Err(self.optix_error("rtTransformSetChild", result));
+                }
+            }
+            TransformChild::GeometryGroup(ggh) => {
+                let child_rt_geogrp =
+                    self.ga_geometry_group_obj.get(ggh).unwrap();
+                let result = unsafe {
+                    rtTransformSetChild(rt_xform, *child_rt_geogrp as RTobject)
+                };
+                if result != RtResult::SUCCESS {
+                    return Err(self.optix_error("rtTransformSetChild", result));
+                }
+            }
+            TransformChild::Transform(th) => {
+                let child_rt_xf = self.ga_transform_obj.get(th).unwrap();
+                let result = unsafe {
+                    rtTransformSetChild(rt_xform, *child_rt_xf as RTobject)
+                };
+                if result != RtResult::SUCCESS {
+                    return Err(self.optix_error("rtTransformSetChild", result));
+                }
+            }
+            TransformChild::None => unreachable!(),
+        }
+
+        self.gd_transform_child.insert(xform, child);
+
+        Ok(xform)
+    }
+
     pub fn transform_destroy(&mut self, xform: TransformHandle) {
         let rt_xform = self.ga_transform_obj.remove(xform).unwrap();
         let _child = self.gd_transform_child.remove(xform);
