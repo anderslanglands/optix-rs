9118830580cbc3c957d9a4ff97a5072d9f265d6c
Anders Langlands <anderslanglands@gmail.com>
2019-03-31 13:31:08 +1300

    cuda lib path to lib64

--- build.rs
+++ build.rs
@@ -53,5 +53,5 @@ fn main() {
 
     println!("cargo:rustc-link-lib=dylib=optix");
     println!("cargo:rustc-link-search=native={}/lib64", optix_root);
-    println!("cargo:rustc-link-search=native={}/lib", cuda_root);
+    println!("cargo:rustc-link-search=native={}/lib64", cuda_root);
 }
